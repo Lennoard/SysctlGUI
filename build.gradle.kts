3cd35ab4d28d7eec7dd6ff96c686a1c4e41e5bcf
Lennoard <lennoardrai@gmail.com>
2023-07-27 17:53:40 -0300

    refactor: buildSrc + SDK 33

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlinVersion = "1.6.21"
-
     repositories {
         google()
         mavenCentral()
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        classpath(BuildPlugins.gradle)
+        classpath(BuildPlugins.kotlin)
     }
 }
 
---
+++ buildSrc/src/main/kotlin/AndroidX.kt
@@ -0,0 +1,18 @@
+object AndroidX {
+    const val appCompat = "androidx.appcompat:appcompat:1.6.1"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.4"
+    const val core = "androidx.core:core-ktx:1.10.1"
+    const val splashScreen = "androidx.core:core-splashscreen:1.0.1"
+    const val lifecycleLiveData = "androidx.lifecycle:lifecycle-livedata-ktx:2.6.1"
+    const val preference = "androidx.preference:preference-ktx:1.2.0"
+    const val swipeRefreshLayout = "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0"
+
+    private const val navigationVersion = "2.6.0"
+    const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:$navigationVersion"
+    const val navigationUi = "androidx.navigation:navigation-ui-ktx:$navigationVersion"
+
+    private const val roomVersion = "2.5.2"
+    const val room = "androidx.room:room-ktx:$roomVersion"
+    const val roomRuntime = "androidx.room:room-runtime:$roomVersion"
+    const val roomCompiler = "androidx.room:room-compiler:$roomVersion"
+}
