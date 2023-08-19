854bc7162f81a75ce4c4bf2559d7729204fd4076
Lennoard <lennoardrai@gmail.com>
2023-08-19 11:55:38 -0300

    release: [2.1.1] versionCode 13
    Signed-off-by: Lennoard <lennoardrai@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = AppConfig.appId
         minSdk = AppConfig.minSdkVersion
         targetSdk = AppConfig.targetSdlVersion
-        versionCode = 12
-        versionName = "2.1.0"
+        versionCode = 13
+        versionName = "2.1.1"
         vectorDrawables.useSupportLibrary = true
         resourceConfigurations.addAll(listOf("en", "de", "pt-rBR"))
         javaCompileOptions {
