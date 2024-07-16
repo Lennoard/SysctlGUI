46f27e3a1c81ab083b1adf6f75e058c7b4ade03a
lennoard <lennoardrai@gmail.com>
2024-07-16 14:10:36 -0300

    release: [2.2.2] versionCode 16
    Signed-off-by: lennoard <lennoardrai@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
 }
 
 buildscript {
