4e54e9ae3b3ccb4c36ce8072ce1ef908fce9fd82
lennoard <lennoardrai@gmail.com>
2026-03-03 21:41:51 -0300

    build: upgrade to JDK 21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,11 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
